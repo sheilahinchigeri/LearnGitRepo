8ed9960dd505c2137234899875c8e82a7e0fd605
Sheila <sheilahinchigeri@gmail.com>
2021-11-10 14:02:06 -0600

    adding max feature to minmax.java from branch C2

--- minmax.java
+++ minmax.java
@@ -5,4 +5,13 @@ public static int minFunction(int n1, int n2){
 	else
 		min=n1;
 	return min;
+
+}
+public static int maxfunction(int n1, int n2){
+	int max;
+	if(n1<n2)
+		max=n2;
+	else
+		max=n1;
+	return max;
 }
